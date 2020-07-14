c699667dd9fc3cde2d1f5eef93b339c4ae1cef4c
jonathan lopez <jdavelg@gmail.com>
2020-07-14 12:07:41 -0600

    deleted one migration

--- database/migrations/2020_06_15_225809_create_categories_table.php
+++ database/migrations/2020_06_15_225809_create_categories_table.php
@@ -14,7 +14,7 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id()->index();
+            $table->id();
             $table->string('name')->nullable($value=true);
           
             $table->timestamps();
